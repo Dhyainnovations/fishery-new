6f17c9a050ba47a2f0c2955840e9a3a661dd3de8
unknown <info@dhya.in>
2022-02-16 15:05:16 +0530

    POS-Fishery

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+import { ToastController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+  constructor() {
+  
+  }
+
+  ngOnInit() {
+
+  }
+
+}
---
+++ src/app/tab5/tab5.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+import { ToastController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-tab5',
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss']
+})
+export class Tab5Page {
+
+  constructor() {
+ 
+  }
+
+
+  
+
+}
