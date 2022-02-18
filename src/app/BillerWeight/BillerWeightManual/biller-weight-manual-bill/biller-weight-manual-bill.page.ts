b6592ed6c54ed6252521f43b11bcdffb2bc1e9cc
Dhyainnovations <97655968+Dhyainnovations@users.noreply.github.com>
2022-02-18 14:59:13 +0530

    updated v/GK

--- src/app/BillerWeight/BillerWeightManual/biller-weight-manual-bill/biller-weight-manual-bill.page.ts
+++ src/app/BillerWeight/BillerWeightManual/biller-weight-manual-bill/biller-weight-manual-bill.page.ts
@@ -179,84 +179,83 @@ export class BillerWeightManualBillPage implements OnInit {
 
   onSuccess() {
     alert("Successfully Printed");
-    //Data to be printed presented in jsonData format.....
+    const items = item => ({
+      quality: item.quality,
+      weight: item.weight,
+      price: item.price,
+      totalcost: item.totalcost,
+    })
+    let product = this.jsonData.map(items)
 
-    // const items = item => ({
-    //   quality: item.quality,
-    //   weight: item.weight,
-    //   price: item.price,
-    //   totalcost: item.totalcost,
-    // })
-    // let product = this.jsonData.map(items)
+    //Calculate the total price of the items in an object
+    let totalPrice = this.totalsum
 
-    // //Calculate the total price of the items in an object
-    // let totalPrice = this.totalsum
+    let company = "Sakthi & Co"
+    let counter = this.billWeightData.counter
+    let biller = this.name
+    let time = this.purchaseddate;
+    //let amoutntReceived = 400
+    //let change = amoutntReceived - totalPrice
 
-    // let company = "Sakthi & Co"
-    // let counter = this.billWeightData.counter
-    // let biller = this.name
-    // let time = this.purchaseddate;
-    // //let amoutntReceived = 400
-    // //let change = amoutntReceived - totalPrice
+    let receipt = ""
+    receipt += commands.TEXT_FORMAT.TXT_WIDTH[1]
+    receipt += "\x1b\x45\x01 \x00" + company + "\x1b\x45\x00"
+    receipt += '\n'
+    receipt += "\x00" + time + "\x00"
 
-    // let receipt = ""
-    // receipt += commands.TEXT_FORMAT.TXT_WIDTH[1]
-    // receipt += "\x1b\x45\x01 \x00" + company + "\x1b\x45\x00"
-    // receipt += '\n'
-    // receipt += "\x00" + time + "\x00"
+    receipt += '\n'
+    receipt += commands.TEXT_FORMAT.TXT_NORMAL
+    receipt += commands.HORIZONTAL_LINE.HR_58MM
+    receipt += '\n'
+    receipt += commands.TEXT_FORMAT.TXT_NORMAL
+    receipt += '\x1B' + '\x61' + '\x30'// left align
+    receipt += vsprintf("%-17s %3s %10s\n", ["Counter", "", counter])
+    receipt += vsprintf("%-17s %3s %10s\n", ["Biller", "", biller])
+    receipt += commands.TEXT_FORMAT.TXT_ALIGN_RT
+    receipt += commands.TEXT_FORMAT.TXT_FONT_A
+    receipt += commands.HORIZONTAL_LINE.HR2_58MM
+    receipt += '\n'
+    receipt += commands.TEXT_FORMAT.TXT_ALIGN_LT
+    receipt += '\x1b\x45\x01' + vsprintf("%-17s %3s %10s \n", ["Item", "", "Price"]) + "\x1b\x45\x01"
 
-    // receipt += '\n'
-    // receipt += commands.TEXT_FORMAT.TXT_NORMAL
-    // receipt += commands.HORIZONTAL_LINE.HR_58MM
-    // receipt += '\n'
-    // receipt += commands.TEXT_FORMAT.TXT_NORMAL
-    // receipt += '\x1B' + '\x61' + '\x30'// left align
-    // receipt += vsprintf("%-17s %3s %10s\n", ["Counter", "", counter])
-    // receipt += vsprintf("%-17s %3s %10s\n", ["Biller", "", biller])
-    // receipt += commands.TEXT_FORMAT.TXT_ALIGN_RT
-    // receipt += commands.TEXT_FORMAT.TXT_FONT_A
-    // receipt += commands.HORIZONTAL_LINE.HR2_58MM
-    // receipt += '\n'
+    for (var pro in product) {
+      if (product.hasOwnProperty(pro)) {
+        var item = product[pro]
+        var itemquality = item.quality
+        var itemweight = item.weight
+        var itemperkg = item.price
+        var itemtotal = item.totalcost
+        receipt += vsprintf("%-17s %3s %10.2f\n", [this.formatTextWrap(itemquality, 16), "", "Rs."+itemtotal+"/-"])
+        receipt += '\x1b\x61\x00' + "-" + " " + itemweight + " Kgs" +  '\x1b\x61\x00' 
+        receipt += '\n'
+        receipt += '\x1b\x61\x00' + "-" + "Rs." + itemperkg + "/-" + " Per/Kg" +  '\x1b\x61\x00' 
+        receipt += '\n'
+
+      }
+      receipt += '\n'
+    }
     // receipt += commands.TEXT_FORMAT.TXT_ALIGN_LT
-    // receipt += '\x1b\x45\x01' + vsprintf("%-17s %3s %10s \n", ["Item", "", "Price"])
-
-    // for (var pro in product) {
-    //   if (product.hasOwnProperty(pro)) {
-    //     var item = product[pro]
-    //     var itemquality = item.quality
-    //     var itemweight = item.weight
-    //     var itemperkg = item.price
-    //     var itemtotal = item.totalcost
-    //     receipt += vsprintf("%-17s %3s %10.2f\n", [this.formatTextWrap(itemquality, 16), "", itemtotal])
-    //     receipt += '\x1b\x61\x00' + "-" + " " + itemweight + " Kg"
-    //     receipt += '\n'
-    //     receipt += '\x1b\x61\x00' + "-" + " " + itemperkg + " Per/Kg"
-    //     receipt += '\n'
-
-    //   }
-    //   receipt += '\n'
-    // }
-    // // receipt += commands.TEXT_FORMAT.TXT_ALIGN_LT
-    // // receipt += commands.TEXT_FORMAT.TXT_FONT_A
-    // // receipt += commands.HORIZONTAL_LINE.HR2_58MM
-    // // receipt += vsprintf("%-17s %3s %10.2f\n", ["Total Price", "", totalPrice])
-    // receipt += '\n'
-    // receipt += commands.TEXT_FORMAT.TXT_NORMAL
-    // receipt += '\x1B' + '\x61' + '\x30'// left align
-    // receipt += '\x1b\x45\x01' + vsprintf("%-17s %3s %10s\n", ["Total Price", "", totalPrice])
-
-
-
-    // receipt += commands.TEXT_FORMAT.TXT_ALIGN_RT
-
-    // receipt += '\n'
     // receipt += commands.TEXT_FORMAT.TXT_FONT_A
     // receipt += commands.HORIZONTAL_LINE.HR2_58MM
-    // receipt += '\n'
-    // receipt += commands.TEXT_FORMAT.TXT_FONT_B
-    // receipt += '\x1b\x61\x01' + 'Thank you, visit again!' + '\x0a\x0a\x0a\x0a' //The unicode symbols are for centering the text
-    // receipt += "\x1b\x45\x01 \x00" // Full cut paper
-    // this.printText(receipt)
+    // receipt += vsprintf("%-17s %3s %10.2f\n", ["Total Price", "", totalPrice])
+    receipt += '\n'
+    receipt += commands.HORIZONTAL_LINE.HR2_58MM
+    receipt += commands.TEXT_FORMAT.TXT_NORMAL   
+    receipt += '\x1B' + '\x61' + '\x30'// left align
+    receipt += '\x1b\x45\x01' + vsprintf("%-17s %3s %10s\n", ["Total Price", "", "Rs."+totalPrice+"/-"]) + '\x1b\x45\x01'
+
+
+
+    receipt += commands.TEXT_FORMAT.TXT_ALIGN_RT
+
+    receipt += '\n'
+    receipt += commands.TEXT_FORMAT.TXT_FONT_A
+    receipt += commands.HORIZONTAL_LINE.HR2_58MM
+    receipt += '\n'
+    receipt += commands.TEXT_FORMAT.TXT_FONT_B
+    receipt += '\x1b\x61\x01' + 'Thank you, visit again!' + '\x0a\x0a\x0a\x0a' //The unicode symbols are for centering the text
+    receipt += "\x1b\x45\x01 \x00" // Full cut paper
+    this.printText(receipt)
   }
 
   onError(error) {
